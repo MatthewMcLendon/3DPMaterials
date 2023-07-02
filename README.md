03ba17f7cd735ca7a8442cee00d1508614a6649d
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-07-02 16:08:16 -0500

    func: created initial material class and extension

---
+++ README.md
@@ -0,0 +1,5 @@
+# 3D Printing Material Catalog
+
+This app was made as a learning exercise for working with classes.
+
+When working with 3D printing you need to keep track of the different materials you use and what settings work best for those materials. This app provides a convient way to look up materials for easy reference.
---
+++ scripts/materials/material.js
@@ -0,0 +1,51 @@
+class Material {
+  constructor(material) {
+    this.title = material.title;
+    this.type = material.type;
+    this.settings = material.settings;
+    this.storeLink = material.storeLink;
+    this.price = material.price;
+    this.stock = material.stock;
+    this.id = material.id;
+  }
+
+  update(updatedMaterial) {
+    return fetch(`http://localhost:8088/materials/${this.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedMaterial),
+    });
+  }
+
+  delete() {
+    return fetch(`http://localhost:8088/materials/${this.id}`, {
+      method: "DELETE",
+    });
+  }
+}
+
+export class Resin extends Material {
+  constructor(material) {
+    super(material);
+  }
+
+  render() {
+    return `
+    <h2>Test ${this.title}</h2>
+    `;
+  }
+}
+
+export class Fdm extends Material {
+  constructor(material) {
+    super(material);
+  }
+
+  render() {
+    return `
+    <h2>Test ${this.title}</h2>
+    `;
+  }
+}
